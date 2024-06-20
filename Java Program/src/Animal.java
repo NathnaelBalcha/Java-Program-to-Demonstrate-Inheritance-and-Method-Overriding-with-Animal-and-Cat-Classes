9e97d41dd9eb5892019ec89d8a2c9bec0f409660
NathnaelBalcha <162681707+NathnaelBalcha@users.noreply.github.com>
2024-06-20 09:35:26 +0300

    Add files via upload

---
+++ Java Program/src/Animal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Nathnanel Balcha
+ */
+class Animal {
+
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
---
+++ Java Program/src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Nathnanel Balcha
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        myAnimal.makeSound();
+        Cat myCat = new Cat();
+        myCat.makeSound();
+    }
+}
